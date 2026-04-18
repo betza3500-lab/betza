6a1519bd99f43bb806eb54214ef5bfc0e4e81d0a
betza <betza3500@gmail.com>
2026-04-18 15:46:27 +0200

    static website cannot proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,22 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  const apiUrl = mode === 'production' 
-    ? 'http://betza-api.onrender.com/api'
-    : 'http://localhost:5000/api'
-  
-  return {
-    server: {
-     proxy: { 
-       '/api': apiUrl
-     }
-    },
-    plugins: [vue()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
