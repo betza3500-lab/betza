51795aa88122713f4cb60d834b243cab8fdf60ec
betza <betza3500@gmail.com>
2026-04-18 15:38:08 +0200

    use different backend in production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,16 +4,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-   proxy: { 
-     '/api': 'http://localhost:5000'
-   }
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command, mode }) => {
+  const apiUrl = mode === 'production' 
+    ? 'http://betza-api.onrender.com'
+    : 'http://localhost:5000'
+  
+  return {
+    server: {
+     proxy: { 
+       '/api': apiUrl
+     }
+    },
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
